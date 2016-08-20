4cb7f03b296ccfd227930aa4d19b65ddf43d6c7d
Kot19 <kolya.kovalec@ukr.net>
2016-08-20 09:23:43 +0300

    https://github.com/Kot19/additional__task_1.git

--- dev/src/additional_task_1.java
+++ dev/src/additional_task_1.java
@@ -1,5 +1,12 @@
+import sun.text.resources.lt.JavaTimeSupplementary_lt;
+
 /**
  * Created by Moshe Balanga on 17.08.2016.
  */
-public class additional_task_1 {
+public class additional_task_1{
+    public static void main(String[] args) {
+        System.out.println("mkdir hello" +
+                "cd hello" +
+                "touch hello.html");
+    }
 }
